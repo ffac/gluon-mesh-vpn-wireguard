ecb3748fab370b930f59bdddd4e15d6611cde8ab
Florian Maurer <maurer@fh-aachen.de>
2022-08-18 00:20:19 +0200

    add wg-registration package to publish wg-key

---
+++ ff-wg-registration/files/lib/gluon/wg-registration/registration.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+wg show | grep transfer
+if [ "$?" -ne "0" ]; then
+        logger -t wg-registration "No connection - trying to register"
+        # Push public key to broker, test for https and use if supported
+        wget -q https://[::1]
+        if [ $? -eq 1 ]; then
+                PROTO=http
+        else
+                PROTO=https
+        fi
+        PUBLICKEY=$(uci get network.wg_mesh.private_key | wg pubkey)
+        NODENAME=$(uci get system.@system[0].hostname)
+        BROKER=$(uci get wireguard.mesh_vpn.broker)
+        logger -t wg-registration "Post $NODENAME and $PUBLICKEY to $PROTO://$BROKER"
+	gluon-wan wget -q  -O- --post-data='{"node_name": "'"$NODENAME"'","public_key": "'"$PUBLICKEY"'"}' $PROTO://$BROKER
+fi
